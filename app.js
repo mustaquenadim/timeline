255bbc3bad9a8dee165080f698a9aecb3ade7caf
mustaquenadim <mustaquenadim2000@gmail.com>
2022-08-28 17:51:48 +0600

    animation added adn event defined

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
-var swiper = new Swiper(".mySwiper", {
+// Initialize Swiper
+const swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     slidesPerView: 1,
     spaceBetween: 30,
@@ -8,4 +8,14 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         clickable: true,
     },
+});
+
+// Swiper slideChange functionality
+swiper.on('slideChange', function (swiper) {
+    const slide = document.getElementsByClassName('animate__animated')[swiper.activeIndex];
+    const prevSlide = document.getElementsByClassName('animate__animated')[swiper.activeIndex - 1];
+    const nextSlide = document.getElementsByClassName('animate__animated')[swiper.activeIndex + 1];
+    slide.classList.add('animate__backInUp');
+    prevSlide.classList.remove('animate__backInUp');
+    nextSlide.classList.remove('animate__backInUp');
 });
